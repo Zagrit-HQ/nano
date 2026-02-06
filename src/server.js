14ba70ef77a7fae30ac7acaf30d99e678829c268
Wito <WitoDelnat@users.noreply.github.com>
2026-02-06 16:02:06 +0100

    change b

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.get("/ready", (_, response) => {
 });
 app.get("/foo", (_, result) => {
   console.log("GET /foo 200");
-  result.json({ message: "baz a" });
+  result.json({ message: "baz b" });
 });
 app.post("/hello", (request, response) => {
   console.log("POST /hello 200");
