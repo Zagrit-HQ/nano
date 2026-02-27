3fdfa09b27851c77f729d6a28564c0759f47c996
wito <wito@depot.dev>
2026-02-27 14:36:33 +0100

    chore: simulate dev activity

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.get("/ready", (_, response) => {
 });
 app.get("/foo", (_, result) => {
   console.log("GET /foo 200");
-  result.json({ message: "baz i" });
+  result.json({ message: "baz: <value>fdsafsafdsa</value>" });
 });
 app.post("/hello", (request, response) => {
   console.log("POST /hello 200");
